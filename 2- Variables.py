74c2278fcc1515f6a4c4275f504e9ef8bb0f3961
imf93 <imerinofajardo@gmail.com>
2023-07-08 09:12:02 +0200

    Main

---
+++ 2- Variables.py
@@ -0,0 +1,11 @@
+my_string_variable = "Esto es una variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = True
+
+print(my_bool_variable)
+
+print(my_string_variable,my_int_variable,my_bool_variable)
